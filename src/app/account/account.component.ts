59feef44027fa0bded30e07c67b2790d81cd4611
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-18 16:42:18 +0300

    add routing in app.comp.html update get data function

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { User } from '../user-class/user';
 import { Repo } from '../repo-class/repo';
 import { AccountService } from '../account-service/account.service';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-account',
@@ -12,26 +12,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AccountComponent implements OnInit {
 
-  userName: string = '';
-  isError: boolean = false;
-  userNameLink: any;
-  userData: Users;
-  showUserDetails: boolean = false;
-  repoData: Repo[];
+  userData: User;
+  myData: {};
+  userRepoData: User[];
+  searchTerm: string = '';
 
   constructor(private router: ActivatedRoute, private accountService: AccountService){}
   
 
-  ngOnInit() {
-    interface ApiResponse{
-      user:string;
-      repo:string;
-    }
-    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
-      // Succesful API request
-      this.account = new Account(data.user, data.repo)
-    })  
+  ngOnInit(): void {}
   
+  getUserData() {
+    this.accountService.getUserData(this.searchTerm).then((data) => {
+      this.userData = data;
+      this.router.navigate(['/searchUser', this.userData.login]);
+    });
   }
-
 }
