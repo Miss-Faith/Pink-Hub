914732ad295531a608e333147da225520d4c3f48
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-18 16:12:46 +0300

    Update user class constructor

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,3 +1,14 @@
 export class User {
-  constructor(public user:string){}
+  constructor(
+    public bio: any,
+    public public_repos: number,
+    public followers: any,
+    public following: any,
+    public created_at: Date,
+    public url: any,
+    public login: string,
+    public html_url: any,
+    public location: any,
+    public avatar_url?: any 
+  ){}
 }
