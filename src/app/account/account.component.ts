f9b4fbe9829c655be6262c32417d1df95e9121cb
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-18 11:21:54 +0300

    Add services

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user'
+import { Repository } from '../repository'
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+
+   constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
