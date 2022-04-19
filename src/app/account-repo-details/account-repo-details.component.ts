4363dfe258b6876b10b4cd339682d06eaa5a26b6
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-19 21:48:00 +0300

    update user and repos

--- src/app/account-repo-details/account-repo-details.component.ts
+++ src/app/account-repo-details/account-repo-details.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AccountService } from '../account-service/account.service';
-import { Repo } from '../repo-class/repo';
+
 
 @Component({
   selector: 'app-account-repo-details',
   templateUrl: './account-repo-details.component.html',
   styleUrls: ['./account-repo-details.component.css'],
-  providers: [AccountService]
 })
+
 export class AccountRepoDetailsComponent implements OnInit {
-  @ViewChild('f') searchForm!: NgForm;
+  @ViewChild('repoForm') searchRepoForm: NgForm;
   
   repo: any;
   searchText:string;
-  reposUserService: any;
-
+  
     constructor(public accountservice: AccountService ) { }
 
   ngOnInit() {}
@@ -23,7 +22,7 @@ export class AccountRepoDetailsComponent implements OnInit {
 
   //search for github repositories
   searchGithubRepos() {
-    this.searchText = this.searchForm.value.value.search;
+    this.searchText = this.searchRepoForm.value.value.search;
     this.accountservice.gitUserRepos(this.searchText).then(
       (response) => {
         this.repo = this.accountservice.getRepoDetails;
